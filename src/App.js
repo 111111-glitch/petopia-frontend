4e9d077a7f124c064c4aee67ccff9d1391684a0a
Leila Abdi <leila.abdi@student.moringaschool.com>
2024-05-20 12:02:46 +0300

    navabar,landing page,checkout

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Home from './components/Home';
 import Login from './components/Login';
 import Register from './components/Register';
 import Products from './components/Products';
+import Cart from './components/Cart';
+import Checkout from './components/Checkout';
 import Services from './components/Services';
 import { Routes, Route } from 'react-router-dom';
 
@@ -16,6 +18,8 @@ function App() {
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
         <Route path='/products' element={<Products />} />
+        <Route path='/Cart' element={<Cart />} />
+        <Route path='/Checkout' element={<Checkout />} />
         <Route path='/services' element={<Services />} />
       </Routes>
     </div>
