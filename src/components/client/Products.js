5373cccd52bf8241524a1b0437e87ba35da64326
joseph <michealkinuthia.m.k@gmail.com>
2024-05-24 10:47:45 +0300

    updates in pages for product,service

--- src/components/client/Products.js
+++ src/components/client/Products.js
@@ -1,36 +1,28 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Product.css';
 
-
-const Products = () => {
+const Products = ({ addItemToCart }) => {
   const [products, setProducts] = useState([]);
-  const [cartItems, setCartItems] = useState([]);
-  const [selectedProduct, setSelectedProduct] = useState(null);
   const [sortOption, setSortOption] = useState('Default');
+  const navigate = useNavigate();
 
   useEffect(() => {
-    
-
-  fetch('/userproducts')
-  .then(response =>response.json())
-  .then(data =>{
-    const filteredProducts= data.filter(item => item.type === 'product');
-    setProducts(filteredProducts);
-  });
-},[]);
+    fetch('/userproducts')
+      .then(response => response.json())
+      .then(data => {
+        const filteredProducts = data.filter(item => item.type === 'product');
+        setProducts(filteredProducts);
+      });
+  }, []);
 
   const handleProductClick = (product) => {
-    setSelectedProduct(product);
+    navigate(`/product/${product.id}`);
   };
 
   const handleAddToCart = (product) => {
-    setCartItems([...cartItems, product]);
-    console.log('Added to cart:', product);
+    addItemToCart(product.id, 1); // Assuming default quantity is 1
   };
-
-  // const handleRemoveFromCart = (product) => {
-  //   setCartItems(cartItems.filter((item) => item.id !== product.id));
-  // };
 
   const handleSort = (event) => {
     const option = event.target.value;
@@ -57,38 +49,26 @@ const Products = () => {
 
   return (
     <div>
-      {/* <div className="flexColStart p-head">
-        <span className='orangeText'>Best Choices</span>
-        <span className='primaryText'>Popular Categories</span>
-      </div> */}
       <h2>PRODUCTS</h2>
       <select value={sortOption} onChange={handleSort}>
-        <option value="Default">Default Sorting</option>
-        <option value="Title">Sort By Name</option>
+        <option value="Default">DEFAULT VIEW</option>
+        <option value="Title">VIEW BY NAME</option>
         <option value="Price-low">By Price: low to high</option>
-        <option value="Price-high">Sort By Price: high to low</option>
+        <option value="Price-high">Price: high to low</option>
       </select>
       <div className="product-grid">
         {products.map((product) => (
-          <div key={product.id}>
+          <div key={product.id} className="product-item">
             <div onClick={() => handleProductClick(product)}>
-            <img src={product.image_url} alt={product.name} />
+              <img src={product.image_url} alt={product.name} />
               <h3>{product.name}</h3>
               <p>{product.description}</p>
-              <p>Price: ${product.price}</p>
-              
+              <p>Price: kes{product.price}</p>
             </div>
-            {selectedProduct && selectedProduct.id === product.id && (
-              <div>
-                <p>{product.description}</p>
-                <button onClick={() => handleAddToCart(product)}>Add to Cart</button>
-                
-              </div>
-            )}
+            <button onClick={() => handleAddToCart(product)}>Add to Cart</button>
           </div>
         ))}
       </div>
-      
     </div>
   );
 };
